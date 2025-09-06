9f0e3f4dadc5cd2d9a23860a396feaf422e79b07
januarpancaran <januar352@gmail.com>
2025-09-06 15:58:31 +0700

    feat: adding lspsaga.nvim, nvim-lightbulb, and nvim-docs-view

---
+++ nvim/lua/plugins/lspsaga-nvim.lua
@@ -0,0 +1,10 @@
+return {
+	"nvimdev/lspsaga.nvim",
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter",
+		"nvim-treesitter/nvim-web-devicons",
+	},
+	config = function()
+		require("lspsaga").setup()
+	end,
+}
