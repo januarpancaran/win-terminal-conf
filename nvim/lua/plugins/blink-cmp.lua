5cb6fb95e58823566b66a82bce7af677520f481f
januarpancaran <januar352@gmail.com>
2025-09-06 20:51:18 +0700

    feat: adding blink.cmp and copilot.lua

---
+++ nvim/lua/plugins/blink-cmp.lua
@@ -0,0 +1,100 @@
+return {
+  "saghen/blink.cmp",
+  version = "1.*",
+  dependencies = {
+    "rafamadriz/friendly-snippets",
+    "giuxtaposition/blink-cmp-copilot",
+  },
+  ---@module 'blink.cmp'
+  ---@type blink.cmp.Config
+  opts = {
+    keymap = {
+      preset = "none",
+
+      ["<CR>"] = { "select_and_accept", "fallback" },
+      ["<C-e>"] = { "hide" },
+      ["<Tab>"] = { "select_next", "fallback" },
+      ["<S-Tab>"] = { "select_prev", "fallback" },
+      ["<C-d>"] = { "scroll_documentation_up", "fallback" },
+      ["<C-f>"] = { "scroll_documentation_down", "fallback" },
+    },
+
+    appearance = {
+      nerd_font_variant = "normal",
+      kind_icons = {
+        Copilot = "",
+        Text = "󰉿",
+        Method = "󰊕",
+        Function = "󰊕",
+        Constructor = "󰒓",
+
+        Field = "󰜢",
+        Variable = "󰆦",
+        Property = "󰖷",
+
+        Class = "󱡠",
+        Interface = "󱡠",
+        Struct = "󱡠",
+        Module = "󰅩",
+
+        Unit = "󰪚",
+        Value = "󰦨",
+        Enum = "󰦨",
+        EnumMember = "󰦨",
+
+        Keyword = "󰻾",
+        Constant = "󰏿",
+
+        Snippet = "󱄽",
+        Color = "󰏘",
+        File = "󰈔",
+        Reference = "󰬲",
+        Folder = "󰉋",
+        Event = "󱐋",
+        Operator = "󰪚",
+        TypeParameter = "󰬛",
+      },
+    },
+
+    completion = {
+      documentation = { auto_show = true },
+      menu = {
+        auto_show = true,
+        draw = {
+          columns = {
+            { "label",     "label_description", gap = 1 },
+            { "kind_icon", "kind" },
+          },
+        },
+      },
+      ghost_text = {
+        enabled = true,
+        show_with_menu = true,
+      },
+    },
+
+    sources = {
+      default = { "lsp", "path", "snippets", "buffer", "copilot" },
+      providers = {
+        copilot = {
+          name = "copilot",
+          module = "blink-cmp-copilot",
+          score_offset = 100,
+          async = true,
+          transform_items = function(_, items)
+            local CompletionItemKind = require("blink.cmp.types").CompletionItemKind
+            local kind_idx = #CompletionItemKind + 1
+            CompletionItemKind[kind_idx] = "Copilot"
+            for _, item in ipairs(items) do
+              item.kind = kind_idx
+            end
+            return items
+          end,
+        },
+      },
+    },
+
+    fuzzy = { implementation = "prefer_rust_with_warning" },
+  },
+  opts_extend = { "sources.default" },
+}
