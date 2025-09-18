d282fab0df9e75e55822723f8e1f22e853bf477d
januarpancaran <januar352@gmail.com>
2025-09-18 21:37:22 +0700

    update: updating lsp config

--- nvim/lua/plugins/lspconfig-nvim.lua
+++ nvim/lua/plugins/lspconfig-nvim.lua
@@ -1,95 +1,89 @@
 return {
-	{
-		"mason-org/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
+  "mason-org/mason.nvim",
+  dependencies = {
+    "mason-org/mason-lspconfig.nvim",
+    "neovim/nvim-lspconfig",
+  },
+  config = function()
+    require("mason").setup()
 
-	{
-		"mason-org/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"bashls",
-					"clangd",
-					"cmake",
-					"csharp_ls",
-					"cssls",
-					"gopls",
-					"html",
-					"intelephense",
-					"jdtls",
-					"jsonls",
-					"lua_ls",
-					"marksman",
-					"pyright",
-					"rust_analyzer",
-					"sqls",
-					"svelte",
-					"tailwindcss",
-					"terraformls",
-					"ts_ls",
-					"yamlls",
-				},
-			})
-		end,
-	},
+    require("mason-lspconfig").setup({
+      ensure_installed = {
+        "bashls",
+        "clangd",
+        "cmake",
+        "csharp_ls",
+        "cssls",
+        "gopls",
+        "html",
+        "intelephense",
+        "jdtls",
+        "jsonls",
+        "lua_ls",
+        "marksman",
+        "pyright",
+        "rust_analyzer",
+        "sqls",
+        "svelte",
+        "tailwindcss",
+        "terraformls",
+        "ts_ls",
+        "yamlls",
+      },
+    })
 
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local lspconfig = require("lspconfig")
+    local on_attach = function(_, bufnr)
+      vim.bo[bufnr].omnifunc = "v:lua.vim.lsp.omnifunc"
+    end
 
-			local on_attach = function(_, bufnr)
-				vim.bo[bufnr].omnifunc = "v:lua.vim.lsp.omnifunc"
-			end
+    if vim.lsp.inlay_hint then
+      vim.api.nvim_create_autocmd("LspAttach", {
+        callback = function(args)
+          local client = vim.lsp.get_client_by_id(args.data.client_id)
+          if client.server_capabilities.inlayHintProvider then
+            vim.lsp.inlay_hint.enable(true, { bufnr = args.buf })
+          end
+        end,
+      })
+    end
 
-			if vim.lsp.inlay_hint then
-				vim.api.nvim_create_autocmd("LspAttach", {
-					callback = function(args)
-						local client = vim.lsp.get_client_by_id(args.data.client_id)
-						if client.server_capabilities.inlayHintProvider then
-							vim.lsp.inlay_hint.enable(true, { bufnr = args.buf })
-						end
-					end,
-				})
-			end
+    local servers = {
+      bashls = {},
+      clangd = {},
+      cmake = {},
+      csharp_ls = {},
+      cssls = {},
+      gopls = {},
+      html = {},
+      jdtls = {},
+      lua_ls = {
+        settings = {
+          Lua = {
+            diagnostics = { globals = { "vim" } },
+          },
+        },
+      },
+      marksman = {},
+      intelephense = {},
+      pyright = {},
+      rust_analyzer = {},
+      sqls = {},
+      svelte = {},
+      tailwindcss = {},
+      terraformls = {},
+      ts_ls = {},
+      yamlls = {},
+    }
 
-			local servers = {
-				bashls = {},
-				clangd = {},
-				cmake = {},
-				csharp_ls = {},
-				cssls = {},
-				gopls = {},
-				html = {},
-				jdtls = {},
-				lua_ls = {
-					settings = {
-						Lua = {
-							diagnostics = { globals = { "vim" } },
-						},
-					},
-				},
-				marksman = {},
-				intelephense = {},
-				pyright = {},
-				rust_analyzer = {},
-				sqls = {},
-				svelte = {},
-				tailwindcss = {},
-				terraformls = {},
-				ts_ls = {},
-				yamlls = {},
-			}
+    for server, config in pairs(servers) do
+      local lsp = vim.lsp.config[server]
 
-			for server, config in pairs(servers) do
-				lspconfig[server].setup(vim.tbl_extend("force", {
-					on_attach = on_attach,
-					capabilities = vim.lsp.protocol.make_client_capabilities(),
-				}, config))
-			end
-		end,
-	},
+      if lsp and lsp.setup then
+        lsp.setup(vim.tbl_extend("force", {
+          on_attach = on_attach,
+          capabilities = vim.lsp.protocol.make_client_capabilities(),
+        }, config))
+      end
+    end
+  end,
 }
