ae6505ad4bc4cba9a78dc208cead7e46f7b39947
Januar Pancaran <151476664+januarpancaran@users.noreply.github.com>
2025-10-29 21:28:40 +0700

    chore: fixing indent blankline file name in init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,6 @@
 if vim.g.vscode then
 	require("plugins.comment-nvim")
-	require("plugins.indent-blankline")
+	require("plugins.indent-blankline-nvim")
 	require("plugins.markdown-preview-nvim")
 	require("plugins.nvim-treesitter-context")
 	require("plugins.rainbow-delimiters-nvim")
