baa0dc3edd5f0e11c32fa363f4c146b219edd61a
januarpancaran <januar352@gmail.com>
2026-01-26 22:36:22 +0700

    feat!: remove deprecated neovim and wsl config

--- nvim/lua/plugins/render-markdown-nvim.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"MeanderingProgrammer/render-markdown.nvim",
-	dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-mini/mini.nvim" },
-	---@module 'render-markdown'
-	---@type render.md.UserConfig
-	config = function()
-		require("render-markdown").setup()
-	end,
-}
--- wsl/nvim/init.lua
+++
@@ -1,6 +0,0 @@
-if not vim.g.vscode then
-	require("config.lazy")
-end
-
-require("config.options")
-require("config.mappings")
